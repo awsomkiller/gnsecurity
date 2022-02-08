d2fcb0444e782cbd443728aa572c710b1ec0974c
Ayush kumar <ayushkr08@gmail.com>
2022-02-08 16:12:24 +0530

    Update views.py

--- gnsecurity/views.py
+++ gnsecurity/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import  render
 
-def wait(request):
-    return render(request, 'wait.html')
+def home(request):
+    return render(request, 'index.html')
+
+def contactus(request):
+    return render(request, 'contact.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about(request):
+    return render(request, 'about.html')
