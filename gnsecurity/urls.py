5463cb7834e91f473b9e01089847396fe2412bb7
Ayush kumar <ayushkr08@gmail.com>
2022-02-08 16:14:10 +0530

    Update urls.py

--- gnsecurity/urls.py
+++ gnsecurity/urls.py
@@ -6,5 +6,9 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.wait, name="wait" )
+    path('', views.home, name="home" ),
+    path('about/', views.about, name="about" ),
+    path('gallery/', views.gallery, name="gallery" ),
+    path('services/', views.services, name="services" ),
+    path('contactus/', views.contactus, name="contactus" )
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
